32df51bbfdd7d5a4175f4013143248d3808b5e3d
Joel <j.n.brewster@gmail.com>
2016-06-15 17:41:13 +1000

    All broken

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
+// var express = require('express');
+// var app = express();
+
+// // app.set('view engine' 'pug');
+
+// app.get('/', function (request, response) {
+  //   // console.log(request);
+  //   // responce.render("index", { title: 'Hey', message: 'Hello there!'});
+  //   // response.send("Hello World!");
+  // });
+
+  // app.listen(3000, function () {
+    //   console.log("Example app listening on port 3000");
+    // });
+
 var express = require('express');
 var app = express();
 
 app.set('view engine' 'pug');
 
 app.get('/', function (request, response) {
-  console.log(request);
   response.send("Hello World!");
+  response.render("index", { title: 'Hey', message: 'Hello there!'});
 });
 
 app.listen(3000, function () {
