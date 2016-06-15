eba08a02cd605a9fda9ff894bf6b70c3d4c85b3f
Joel <j.n.brewster@gmail.com>
2016-06-15 14:49:29 +1000

    Added pug

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send("Hello World!");
+app.set('view engine' 'pug');
+
+app.get('/', function (request, response) {
+  console.log(request);
+  response.send("Hello World!");
 });
 
 app.listen(3000, function () {
